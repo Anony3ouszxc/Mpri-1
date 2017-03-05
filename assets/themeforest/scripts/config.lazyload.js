1adaf3a268419c466da532679122edffb5615e70
diffusionzzz <diffusionzzz@gmail.com>
2017-03-05 13:03:25 +0700

    assets file

---
+++ assets/themeforest/scripts/config.lazyload.js
@@ -0,0 +1,72 @@
+// lazyload config
+var MODULE_CONFIG = {
+    easyPieChart:   [ '/assets/themeforest/libs/jquery/jquery.easy-pie-chart/dist/jquery.easypiechart.fill.js' ],
+    sparkline:      [ '/assets/themeforest/libs/jquery/jquery.sparkline/dist/jquery.sparkline.retina.js' ],
+    plot:           [ '/assets/themeforest/libs/jquery/flot/jquery.flot.js',
+                      '/assets/themeforest/libs/jquery/flot/jquery.flot.resize.js',
+                      '/assets/themeforest/libs/jquery/flot/jquery.flot.pie.js',
+                      '/assets/themeforest/libs/jquery/flot.tooltip/js/jquery.flot.tooltip.min.js',
+                      '/assets/themeforest/libs/jquery/flot-spline/js/jquery.flot.spline.min.js',
+                      '/assets/themeforest/libs/jquery/flot.orderbars/js/jquery.flot.orderBars.js'],
+    vectorMap:      [ '/assets/themeforest/libs/jquery/bower-jvectormap/jquery-jvectormap-1.2.2.min.js',
+                      '/assets/themeforest/libs/jquery/bower-jvectormap/jquery-jvectormap.css',
+                      '/assets/themeforest/libs/jquery/bower-jvectormap/jquery-jvectormap-world-mill-en.js',
+                      '/assets/themeforest/libs/jquery/bower-jvectormap/jquery-jvectormap-us-aea-en.js' ],
+    dataTable:      [
+                      '/assets/themeforest/libs/jquery/datatables/media/js/jquery.dataTables.min.js',
+                      '/assets/themeforest/libs/jquery/plugins/integration/bootstrap/3/dataTables.bootstrap.js',
+                      '/assets/themeforest/libs/jquery/plugins/integration/bootstrap/3/dataTables.bootstrap.css'],
+    footable:       [
+                      '/assets/themeforest/libs/jquery/footable/dist/footable.all.min.js',
+                      '/assets/themeforest/libs/jquery/footable/css/footable.core.css'
+                    ],
+    screenfull:     [
+                      '/assets/themeforest/libs/jquery/screenfull/dist/screenfull.min.js'
+                    ],
+    sortable:       [
+                      '/assets/themeforest/libs/jquery/html.sortable/dist/html.sortable.min.js'
+                    ],
+    nestable:       [
+                      '/assets/themeforest/libs/jquery/nestable/jquery.nestable.css',
+                      '/assets/themeforest/libs/jquery/nestable/jquery.nestable.js'
+                    ],
+    summernote:     [
+                      '/assets/themeforest/libs/jquery/summernote/dist/summernote.css',
+                      '/assets/themeforest/libs/jquery/summernote/dist/summernote.js'
+                    ],
+    parsley:        [
+                      '/assets/themeforest/libs/jquery/parsleyjs/dist/parsley.css',
+                      '/assets/themeforest/libs/jquery/parsleyjs/dist/parsley.min.js'
+                    ],
+    select2:        [
+                      '/assets/themeforest/libs/jquery/select2/dist/css/select2.min.css',
+                      '/assets/themeforest/libs/jquery/select2-bootstrap-theme/dist/select2-bootstrap.min.css',
+                      '/assets/themeforest/libs/jquery/select2-bootstrap-theme/dist/select2-bootstrap.4.css',
+                      '/assets/themeforest/libs/jquery/select2/dist/js/select2.min.js'
+                    ],
+    datetimepicker: [
+                      '/assets/themeforest/libs/jquery/eonasdan-bootstrap-datetimepicker/build/css/bootstrap-datetimepicker.css',
+                      '/assets/themeforest/libs/jquery/eonasdan-bootstrap-datetimepicker/build/css/bootstrap-datetimepicker.dark.css',
+                      '/assets/themeforest/libs/js/moment/moment.js',
+                      '/assets/themeforest/libs/jquery/eonasdan-bootstrap-datetimepicker/build/js/bootstrap-datetimepicker.min.js'
+                    ],
+    chart:          [
+                      '/assets/themeforest/libs/js/echarts/build/dist/echarts-all.js',
+                      '/assets/themeforest/libs/js/echarts/build/dist/theme.js',
+                      '/assets/themeforest/libs/js/echarts/build/dist/jquery.echarts.js'
+                    ],
+    bootstrapWizard:[
+                      '/assets/themeforest/libs/jquery/twitter-bootstrap-wizard/jquery.bootstrap.wizard.min.js'
+                    ],
+    fullCalendar:   [
+                      '/assets/themeforest/libs/jquery/moment/moment.js',
+                      '/assets/themeforest/libs/jquery/fullcalendar/dist/fullcalendar.min.js',
+                      '/assets/themeforest/libs/jquery/fullcalendar/dist/fullcalendar.css',
+                      '/assets/themeforest/libs/jquery/fullcalendar/dist/fullcalendar.theme.css',
+                      'scripts/plugins/calendar.js'
+                    ],
+    dropzone:       [
+                      '/assets/themeforest/libs/js/dropzone/dist/min/dropzone.min.js',
+                      '/assets/themeforest/libs/js/dropzone/dist/min/dropzone.min.css'
+                    ]
+  };
